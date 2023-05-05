cd214555dd6d152fa6fd123e10fa950a6d71ef4b
Francisco Alonso <57003023+falonso21@users.noreply.github.com>
2023-05-05 18:03:24 +0200

    Update 🏠HomePage.py

--- 🏠HomePage.py
+++ 🏠HomePage.py
@@ -19,18 +19,6 @@ DESCRIPTION = """
 Data Scientist. "You can have data without information, but you cannot have information without data".
 """
 EMAIL = "falonsof96@gmail.com"
-SOCIAL_MEDIA = {
-    "YouTube": "https://youtube.com/c/codingisfun",
-    "LinkedIn": "https://linkedin.com",
-    "GitHub": "https://github.com",
-    "Twitter": "https://twitter.com",
-}
-PROJECTS = {
-    "🏆 Sales Dashboard - Comparing sales across three stores": "https://youtu.be/Sb0A9i6d320",
-    "🏆 Income and Expense Tracker - Web app with NoSQL database": "https://youtu.be/3egaMfE9388",
-    "🏆 Desktop Application - Excel2CSV converter with user settings & menubar": "https://youtu.be/LzCfNanQ_9c",
-    "🏆 MyToolBelt - Custom MS Excel add-in to combine Python & Excel": "https://pythonandvba.com/mytoolbelt/",
-}
 
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
